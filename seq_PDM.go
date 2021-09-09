6ab035926f04323b597337f4846e9601e5fdd90d
Harsh Kumar <29358488+theharshon@users.noreply.github.com>
2021-09-09 14:27:37 +0530

    Add files via upload

---
+++ seq_PDM.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Download struct {
+	Url           string
+	TargetPath    string
+	TotalSections int
+}
+
+func main() {
+	startTime := time.Now()
+	d := Download{
+		Url:           getUrlInput(),
+		TargetPath:    fileName(),
+		TotalSections: 10,
+	}
+	err := d.Do()
+	if err != nil {
+		log.Fatalf("An error occured while downloading the file: %s\n", err)
+	}
+
+	fmt.Printf("Download completed in %v seconds\n", time.Now().Sub(startTime).Seconds())
+
+}
+
+func getUrlInput() string {
+	var user_url string
+	fmt.Println("Please Enter File URL: ")
+	fmt.Scanln(&user_url)
+	return user_url
+
+}
+func fileName() string {
+	var userfile string
+	fmt.Println("Please Enter File Name: ")
+	fmt.Scanln(&userfile)
+	return userfile
+
+}
+
+func (d Download) Do() error {
+	fmt.Println("Making connection")
+	r, err := d.getNewRequest("HEAD")
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Got %v\n", resp.StatusCode)
+
+	if resp.StatusCode > 299 {
+		return errors.New(fmt.Sprintf("Can't process, response is %v", resp.StatusCode))
+	}
+
+	size, err := strconv.Atoi(resp.Header.Get("Content-Length"))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Size is %v bytes\n", size)
+
+	var sections = make([][2]int, d.TotalSections)
+	eachSize := size / d.TotalSections
+	fmt.Printf("Each size is %v bytes\n", eachSize)
+	// example: if file size is 100 bytes, our section should like:
+	// [[0 10] [11 21] [22 32] [33 43] [44 54] [55 65] [66 76] [77 87] [88 98] [99 99]]
+	fmt.Println(sections)
+
+	for i := range sections {
+		if i == 0 {
+			// starting byte of first section
+			sections[i][0] = 0
+		} else {
+			// starting byte of other sections
+			sections[i][0] = sections[i-1][1] + 1
+		}
+
+		if i < d.TotalSections-1 {
+			// ending byte of other sections
+			sections[i][1] = sections[i][0] + eachSize
+		} else {
+			// ending byte of other sections
+			sections[i][1] = size - 1
+		}
+	}
+
+	fmt.Println(sections)
+
+	for i, s := range sections {
+		d.downloadSection(i, s)
+	}
+
+	return nil
+}
+
+func (d Download) getNewRequest(method string) (*http.Request, error) {
+	r, err := http.NewRequest(
+		method,
+		d.Url,
+		nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	r.Header.Set("User-Agent", "Silly Download Manager v001")
+	return r, nil
+}
+
+func (d Download) downloadSection(i int, s [2]int) error {
+	r, err := d.getNewRequest("GET")
+	if err != nil {
+		return err
+	}
+
+	r.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", s[0], s[1]))
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Downloaded %v bytes for section %v: %v\n", resp.Header.Get("Content-Length"), i, s)
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("section-%v.tmp", i), b, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
